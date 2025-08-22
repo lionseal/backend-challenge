955651d0cae9bfa3fea96d8a1ffd1c5414db6013
León Isla <leon.isla.22@gmail.com>
2025-08-22 15:03:20 +0200

    feat: test cases for report generation job

---
+++ src/jobs/ReportGenerationJob.test.ts
@@ -0,0 +1,83 @@
+import { describe, beforeEach, it, expect } from '@jest/globals';
+import { ReportGenerationJob } from './ReportGenerationJob';
+import { Task } from '../models/Task';
+import workflow from '../fixtures/workflow.json';
+import workflow_incomplete from '../fixtures/workflow_incomplete.json';
+import workflow_failed from '../fixtures/workflow_failed.json';
+
+describe('PolygonAreaJob', () => {
+    let job: ReportGenerationJob;
+
+    it('returns final report', async () => {
+        job = new ReportGenerationJob({
+            findOne: jest.fn().mockResolvedValue(workflow),
+        } as any);
+        const result = await job.run({
+            ...workflow.tasks[workflow.tasks.length - 1],
+            workflow: { workflowId: workflow.workflowId },
+        } as Task);
+        expect(result).toMatchObject({
+            workflowId: workflow.workflowId,
+            tasks: [
+                { taskId: workflow.tasks[0].taskId, type: 'analysis', output: '"Brazil"' },
+                { taskId: workflow.tasks[1].taskId, type: 'area', output: '{"area":8363324.273315565}' },
+                { taskId: workflow.tasks[2].taskId, type: 'notification', output: '{}' },
+            ],
+            finalReport:
+                'Task #1 analysis completed, result:\n' +
+                '"Brazil"\n' +
+                'Task #2 area completed, result:\n' +
+                '{\n' +
+                '  "area": 8363324.273315565\n' +
+                '}\n' +
+                'Task #3 notification completed, result:\n' +
+                '{}',
+        });
+    });
+
+    it('throws if not all other tasks are completed', async () => {
+        job = new ReportGenerationJob({
+            findOne: jest.fn().mockResolvedValue(workflow_incomplete),
+        } as any);
+        await expect(
+            job.run({
+                ...workflow_incomplete.tasks[workflow_incomplete.tasks.length - 1],
+                workflow: { workflowId: workflow_incomplete.workflowId },
+            } as Task)
+        ).rejects.toThrow('Cannot generate report: not all other tasks are completed or failed');
+    });
+
+    it('returns failed tasks', async () => {
+        job = new ReportGenerationJob({
+            findOne: jest.fn().mockResolvedValue(workflow_failed),
+        } as any);
+        const result = await job.run({
+            ...workflow_failed.tasks[workflow_failed.tasks.length - 1],
+            workflow: { workflowId: workflow_failed.workflowId },
+        } as Task);
+        expect(result).toMatchObject({
+            workflowId: workflow_failed.workflowId,
+            tasks: [
+                { taskId: workflow_failed.tasks[0].taskId, type: 'analysis', output: '"Brazil"' },
+                { taskId: workflow_failed.tasks[1].taskId, type: 'area', output: '{"area":8363324.273315565}' },
+                {
+                    taskId: workflow_failed.tasks[2].taskId,
+                    type: 'notification',
+                    output: '{"error":"Simulated email failure","stack":"Error: Simulated email failure\\n    at EmailNotificationJob.run (/backend-challenge/src/jobs/EmailNotificationJob.ts:10:15)\\n    at async TaskRunner.run (/backend-challenge/src/workers/taskRunner.ts:32:32)\\n    at async taskWorker (/backend-challenge/src/workers/taskWorker.ts:18:17)"}',
+                },
+            ],
+            finalReport:
+                'Task #1 analysis completed, result:\n' +
+                '"Brazil"\n' +
+                'Task #2 area completed, result:\n' +
+                '{\n' +
+                '  "area": 8363324.273315565\n' +
+                '}\n' +
+                'Task #3 notification failed, result:\n' +
+                '{\n' +
+                '  "error": "Simulated email failure",\n' +
+                '  "stack": "Error: Simulated email failure\\n    at EmailNotificationJob.run (/backend-challenge/src/jobs/EmailNotificationJob.ts:10:15)\\n    at async TaskRunner.run (/backend-challenge/src/workers/taskRunner.ts:32:32)\\n    at async taskWorker (/backend-challenge/src/workers/taskWorker.ts:18:17)"\n' +
+                '}',
+        });
+    });
+});
