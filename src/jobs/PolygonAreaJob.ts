198617b4b3501e60c030c9a4baae95ccb24836f4
León Isla <leon.isla.22@gmail.com>
2025-08-22 11:43:09 +0200

    feat: inital PolygonAreaJob implementation

---
+++ src/jobs/PolygonAreaJob.ts
@@ -0,0 +1,31 @@
+import { Geometry } from "typeorm";
+import { Task } from "../models/Task";
+import { Job } from "./Job";
+import { area } from "@turf/area";
+
+interface PolygonAreaJobResult {
+  area: number;
+}
+
+export class PolygonAreaJob implements Job {
+  async run(task: Task): Promise<PolygonAreaJobResult> {
+    try {
+      const area = this.calculateArea(JSON.parse(task.geoJson));
+      console.log(`Calculated area for task ${task.taskId}: ${area}`);
+      // Returning an object with area, later it could be extended to include more details if needed
+      // ie: number of nodes, triangles, center, etc.
+      return { area };
+    } catch (error) {
+      console.error(`Error calculating area for task ${task.taskId}:`, error);
+      // taskRunner will handle setting the task status to Failed
+      throw error;
+    }
+  }
+
+  /**
+   * Calculate the area in square meters of a GeoJSON polygon using Turf.js
+   */
+  private calculateArea(geoJson: Geometry): number {
+    return area(geoJson);
+  }
+}
